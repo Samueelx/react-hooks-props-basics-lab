e7c40ff5f7e42817e539e8f180de1b1b93b41ed6
Samueelx <gsammygitau@gmail.com>
2023-01-12 16:06:27 +0300

    rendere the components based on the props they receive.

--- src/data/user.js
+++ src/data/user.js
@@ -1,10 +1,10 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Samuel",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
+    github: "https://github.com/Samueelx",
     linkedin: "https://www.linkedin.com/in/liza/",
   },
 };
